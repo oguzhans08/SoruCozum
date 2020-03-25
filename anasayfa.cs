a0404f2bdb29ab6fb1816abd428430deaa4efc01
Oğuzhan Sağlık <45845015+oguzhans08@users.noreply.github.com>
2020-03-26 02:40:29 +0300

    tatilhaftası1
    Bu hafta picturebox'a önizleme aldırıp, seçtiğimiz resmin imageLocation yani PATH'i alıyoruz. Bu imageLocation'u slash ayarını yapıp çünkü mysql tek /'i görmüyor birleşik yazıyor o yüzden replace ile slash eklettirip doğru path'i verdiğim sürece resmi önizleme ve kayıtlı olarak bana gösteriyor.
    İstatistik kısmında ise testi cevaplayıp testi sonlandırdığımızda doğru ve yanlış cevaplarını update sorgusuyla çözdüğü kullanıcının doğru ve yanlış hanesine ekliyorum bu şekilde ilerleyen zamanlarda bu öğrencinin kaç soru çözdüğü kaç doğru kaç yanlış yaptığı gibi genel ve o öğrenci hakkında doğru yanlış sayısını bilgilenmemize yardımcı olur

--- anasayfa.cs
+++ anasayfa.cs
@@ -7,7 +7,10 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MySql.Data.MySqlClient;
+using MySql.Data;//2
+using System.Data.SqlClient;
+using static Globals;
 
 
 
@@ -18,19 +21,43 @@ namespace SoruProjesiYon
 {
     public partial class anasayfa : MetroForm
     {
+        MySqlConnection con;
+        MySqlCommand cmd;
+        MySqlDataReader dr;
+      
         public anasayfa()
         {
             InitializeComponent();
+
+           
+            con = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=;database=sorucozum;");
+
         }
+
+   
         public string kullaniciAdi { get; set; }
         public string derssecim { get; set; }
-
+        public string cevaps;
         private void Anasayfa_Load(object sender, EventArgs e)
         {
-
+            
+            string imgUrl;
             kullansali.Text = kullaniciAdi;
 
+            MessageBox.Show(kullaniciAdi);
+            cmd = new MySqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM uyeler WHERE kullaniciadi='"+kullaniciAdi+"'";
+            using (MySqlDataReader dr = cmd.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    imgUrl = dr.GetValue(5).ToString();
+                    pictureBox1.ImageLocation = imgUrl;
 
+                }
+            }
         }
 
         private void Btn_turkce_ders_Click(object sender, EventArgs e)
@@ -89,6 +116,50 @@ namespace SoruProjesiYon
 
         private void Button1_Click(object sender, EventArgs e)
         {
+
+        }
+        string imageUplGlb;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            String imageLocation = "";
+
+            try
+            {
+                OpenFileDialog imageUpload = new OpenFileDialog();
+                imageUpload.Filter = "jpg files(*.jpg)|*.jpg| PNG files(*.png)|*.png| All Files(*.*)|*.*";
+
+                if (imageUpload.ShowDialog() == DialogResult.OK)
+                {
+                    
+
+                    imageLocation = imageUpload.FileName;
+                    pictureBox1.ImageLocation = imageLocation;
+                    MessageBox.Show(imageLocation);
+
+                    imageUplGlb = imageLocation;
+                    button3.Visible = true;
+
+
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Resim eklenirken bir hata oluştu.");
+            }
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        { //SQL'de insert ederken \ bu slash işaretini görmüyor ve ne kayıt edersem edeyim slashsiz ekliyordu path'i. Path slashsız olunca da resmi çekmiyordu.
+            //Aşağıda eklemiş olduğum linkten replace ile durumu hallettim.
+            string imgPathSlash = imageUplGlb.Replace("\\", "\\\\"); //https://www.codeproject.com/Questions/1246134/Csharp-textbox-not-contains-backslash-while-insert
+            con.Close();
+            cmd = new MySqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "UPDATE uyeler SET picImg='" + imgPathSlash + "' WHERE kullaniciadi='" + kullaniciAdi+ "'";
+            dr = cmd.ExecuteReader();
+   
+            con.Close();
 
         }
     }
--- istatistik.cs
+++ istatistik.cs
@@ -8,26 +8,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using MySql.Data;//2
+using System.Data.SqlClient;
 
 namespace SoruProjesiYon
 {
     public partial class istatistik : MetroForm
     {
+
+        MySqlConnection con;
+        MySqlCommand cmd;
+        MySqlDataReader dr;
+
+        public string kullaniciAdi { get; set; }
         public int dogrusayi { get; set; }
         public int yanlissayi { get; set; }
         public istatistik()
         {
             InitializeComponent();
+            con = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=;database=sorucozum;");
         }
 
         private void İstatistik_Load(object sender, EventArgs e)
         {
+            MessageBox.Show(kullaniciAdi);
             chart1.Titles.Add("Doğru/Yanlış İstatistiğiniz");
             chart1.Series["s1"].Points.AddXY("Yanlış", yanlissayi);
             chart1.Series["s1"].Points.AddXY("Doğru", dogrusayi);
             lbl_dogrusayisi.Text = dogrusayi.ToString(); 
             lbl_yanlissayisi.Text = yanlissayi.ToString();
+            //dogrucevap
 
+        
+            cmd = new MySqlCommand();
+            con.Open();
+            cmd.Connection = con; //insert
+            cmd.CommandText = "UPDATE uyeler SET dogrucevap='" + lbl_dogrusayisi.Text + "', yanliscevap='" + lbl_yanlissayisi.Text + "' WHERE kullaniciadi='" + kullaniciAdi + "'";
+            dr = cmd.ExecuteReader();
+
+            con.Close();
+
+
+
+
+
+        }
+
+        private void Chart1_Click(object sender, EventArgs e)
+        {
 
         }
     }
